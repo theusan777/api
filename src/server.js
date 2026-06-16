381682a848a9aa4f1260ea326b140a88e466236f
theusan777 <theusant777@icloud.com>
2026-06-16 13:33:44 -0300

    refactor server request handling to include URL routing for GET and POST methods

--- src/server.js
+++ src/server.js
@@ -1,9 +1,17 @@
 import http from "node:http"
 
 const server = http.createServer((req, res) => {
-   const { method } = req
+   const { method, url } = req
 
-   return res.writeHead(201).end("Método: " + method)
+   if (method === "GET" && url === "/products") {
+      return res.writeHead(200).end("Product list")
+   }
+
+   if (method === "POST" && url === "/products") {
+      return res.writeHead(201).end("Product created")
+   }
+
+   return res.writeHead(404).end("Not found")
 })
 
 server.listen(3333)
