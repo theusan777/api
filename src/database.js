57d78522b8e27c29590ab0681f2bf2b4a9ee5eee
theusan777 <theusant777@icloud.com>
2026-06-23 14:08:57 -0300

    initialize database class with persistence method and create initial db.json file

--- src/database.js
+++ src/database.js
@@ -1,5 +1,17 @@
+import fs from "node:fs/promises"
+
+const DATABASE_PATH = new URL("db.json", import.meta.url)
+
 export class Database {
    database = {}
+
+   constructor() {
+      this.persist()
+   }
+
+   persist() {
+    fs.writeFile(DATABASE_PATH, JSON.stringify(this.database))
+   }
 
     insert(table, data) {
         if (Array.isArray(this.database[table])) {
